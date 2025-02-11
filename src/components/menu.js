0cec086d62f3be7979c51207fa0155e8ffb401c3
sachinkarhana1 <sachinabs5511@gmail.com>
2025-02-11 18:00:49 +0530

    update the project page

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,7 +3,7 @@ export default function menu() {
         <div class="w-auto flex flex-col items-end gap-4 px-4 py-2 md:flex-row md:items-center">
             <a href="/" class="md:px-5 hover:underline transition-all ">Home</a>
             <a href="/about" class="md:px-5 hover:underline">About</a>
-            <a href="/" class="md:px-5 hover:underline">Why Seactac</a>
+            <a href="/how-seatac-rental-works" class="md:px-5 hover:underline">Why Seactac</a>
             <a href="/car-lists" class="md:mx-5 setac-primary-button">Booked Now</a>
           </div>   
     `;
